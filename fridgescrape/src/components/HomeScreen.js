9f4e0b3fd1b104f498bf958c2f07da38ad2a92d2
ryantruong25 <75468904+ryan-truong@users.noreply.github.com>
2022-04-09 13:35:27 -0700

    HTML/CSS wireframe

---
+++ fridgescrape/src/components/HomeScreen.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+
+export const HomeScreen = () => {
+  return (
+    <>
+        <div className = 'container'>
+            <div className = 'padding'/>
+            <div className = 'title'>
+                <div className = 'logo'></div>
+                <div className = 'fridgescrape'>FridgeScrape</div>
+            </div>
+            <div className = 'formSpacing'>
+                <Form>
+                    <Form.Group>
+                        <div className = 'formWidth'>
+                            <Form.Control required type="text"> 
+                            </Form.Control>
+                        </div>
+                    </Form.Group>
+                    <div className = 'buttonContainer'>
+                        <Button className = 'rounded-pill' variant = 'dark'>Get Recipes</Button>
+                    </div>
+                </Form>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default HomeScreen;
